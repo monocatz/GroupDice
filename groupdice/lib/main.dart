d608c18c2cadd3f753abd85a4447b0102998cf0e
monocatz <126603181+monocatz@users.noreply.github.com>
2023-03-06 22:16:34 +0100

    Base done :cat:

--- groupdice/lib/main.dart
+++ groupdice/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,8 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: "Let's Dice!",
       home: MyHomePage(),
+      theme:ThemeData(
+        primarySwatch: Colors.deepPurple,
+      )
     );
   }
 }
@@ -20,83 +24,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _selectedOption1 = "Option 1";
-  String _selectedOption2 = "Option A";
-  TextEditingController _textEditingController = TextEditingController();
+  TextEditingController _textEditingController1 = TextEditingController();
+  TextEditingController _textEditingController2 = TextEditingController();
+  TextEditingController _textEditingController3 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Group Dice'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextField(
-              controller: _textEditingController,
-              decoration: InputDecoration(
-                hintText: "Enter text here",
+            Center(
+              child: Text(
+                'Projekt erstellen',
+                style: TextStyle(fontSize: 24.0),
               ),
             ),
             SizedBox(height: 16.0),
-            DropdownButton<String>(
-              value: _selectedOption1,
-              onChanged: (value) {
-                setState(() {
-                  _selectedOption1 = value!;
-                });
-              },
-              items: [
-                DropdownMenuItem(
-                  value: "Option 1",
-                  child: Text("Option 1"),
-                ),
-                DropdownMenuItem(
-                  value: "Option 2",
-                  child: Text("Option 2"),
-                ),
-                DropdownMenuItem(
-                  value: "Option 3",
-                  child: Text("Option 3"),
-                ),
-              ],
+            Text(
+              'Thema Projekt',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 8.0),
+            TextField(
+              maxLength: 25,
+              controller: _textEditingController1,
+              decoration: InputDecoration(
+                hintText: "Wie soll das Projekt heißen?",
+              ),
             ),
             SizedBox(height: 16.0),
-            DropdownButton<String>(
-              value: _selectedOption2,
-              onChanged: (value) {
-                setState(() {
-                  _selectedOption2 = value!;
-                });
-              },
-              items: [
-                DropdownMenuItem(
-                  value: "Option A",
-                  child: Text("Option A"),
-                ),
-                DropdownMenuItem(
-                  value: "Option B",
-                  child: Text("Option B"),
-                ),
-                DropdownMenuItem(
-                  value: "Option C",
-                  child: Text("Option C"),
-                ),
-              ],
+            Text(
+              'Anzahl der Personen (gesamt)',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 8.0),
+            TextField(
+              maxLength: 3,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              controller: _textEditingController2,
+              decoration: InputDecoration(
+                hintText: "Wie viele Teilnehmer:innen gibt es?",
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Maximale Gruppengröße',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 8.0),
+            TextField(
+              maxLength: 3,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              controller: _textEditingController3,
+              decoration: InputDecoration(
+                hintText: "Wie groß sollen die einzelnen Gruppen höchstens sein?",
+              ),
             ),
             SizedBox(height: 32.0),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => NextPage()),
+                  MaterialPageRoute(builder: (context) => SecondPage()),
                 );
               },
               child: Text('Weiter'),
-            ),
+              ),
+            SizedBox(height: 32.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FirstPage()),
+                );
+              },
+              child: Text('Zurück'),
+              ),
           ],
         ),
       ),
@@ -104,15 +113,43 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class NextPage extends StatelessWidget {
+class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Next Page'),
+        title: Text('Attribute festlegen'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Text('This is the next page!'),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Text(
+            'Coming soon ...',
+            style: TextStyle(fontSize: 24.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startseite'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Text(
+            'Coming soon ...',
+            style: TextStyle(fontSize: 24.0),
+          ),
+        ),
       ),
     );
   }
